b4a6a9fe054844bab8854f815922057b0f370ccc
Alexander Pronin <pronin.alx@gmail.com>
2016-04-30 13:59:30 +0300

    Final commit

---
+++ myDevices2-starter/myDevices/Device.swift
@@ -0,0 +1,17 @@
+//
+//  Device.swift
+//  myDevices
+//
+//  Created by Aleksandr Pronin on 4/30/16.
+//  Copyright © 2016 Razeware LLC. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+
+class Device: NSManagedObject {
+
+// Insert code here to add functionality to your managed object subclass
+
+}
